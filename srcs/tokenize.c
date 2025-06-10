830cc36c527ba360a154e474e495e5953317b7dd
datagore <104622881+datagore@users.noreply.github.com>
2025-06-10 17:37:17 +0300

    Make tokenizer return NULL on syntax error

--- srcs/tokenize.c
+++ srcs/tokenize.c
@@ -1,15 +1,5 @@
 #include "../incl/minishell.h"
 
-static int	is_blank(char c)
-{
-	return (c == ' ' || c == '\t' || c == '\n');
-}
-
-static int	is_meta(char c)
-{
-	return (c == '|' || c == '<' || c == '>');
-}
-
 static char	*tokenize_meta(t_shell *s, t_vec *tokens, char *i)
 {
 	const int	paired = i[0] == i[1] && (i[0] == '<' || i[0] == '>');
@@ -41,6 +31,24 @@ static char	*tokenize_word(t_shell *s, t_vec *tokens, char *input)
 	return (input);
 }
 
+static bool	is_syntax_error(t_vec *tokens)
+{
+	char **const	str = (char**) tokens->data;
+	char			*last;
+	size_t			i;
+
+	i = 0;
+	while (++i < tokens->size)
+		if (is_meta(str[i - 1][0]) && is_meta(str[i][0]))
+			return (true);
+	if (tokens->size == 0)
+		return (false);
+	last = str[tokens->size - 1];
+	if (*last == '<' || *last == '>')
+		return (true);
+	return (is_meta(str[0][0]));
+}
+
 t_vec	*tokenize(t_shell *s, char *input)
 {
 	t_vec *const	tokens = vector_new(s, 0);
@@ -53,6 +61,11 @@ t_vec	*tokenize(t_shell *s, char *input)
 			input = tokenize_meta(s, tokens, input);
 		else if (*input != '\0')
 			input = tokenize_word(s, tokens, input);
+	}
+	if (is_syntax_error(tokens))
+	{
+		printf("minishell: syntax error\n");
+		return (NULL);
 	}
 	return (tokens);
 }
