1e9e74615f87df211d31c4a986714e5a3a02403f
oliynykmax <olejnikmax@gmail.com>
2025-06-11 23:27:19 +0300

    echo is now working, -n option is supported, no error messages from echo yet

--- srcs/debug.c
+++ srcs/debug.c
@@ -1,12 +1,13 @@
 #include "../incl/minishell.h"
 
-void debug_mode(t_shell *s, char *input, char **envp)
+void	debug_mode(t_shell *s, char *input, char **envp)
 {
-	char *av_input = string_new(s, input);
-	s->tokens = tokenize(s, av_input);
+	const char		*av_input = string_new(s, input);
+	const char		*bash_args[] = {"bash", "-c", (char *)av_input, NULL};
+
+	s->tokens = tokenize(s, (char *)av_input);
 	printf("Our shell:\n");
 	shell_execute(s);
-	char *bash_args[] = {"bash", "-c", av_input, NULL};
 	printf("Bash:\n");
-	execve("/bin/bash", bash_args, envp);
+	execve("/bin/bash", (char **)bash_args, envp);
 }
