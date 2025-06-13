132df69d4ee4f4a6363803b53c16d8eb841aea9a
oliynykmax <olejnikmax@gmail.com>
2025-06-13 16:04:18 +0300

    cd complete

--- srcs/cd.c
+++ srcs/cd.c
@@ -1,9 +1,21 @@
 #include "../incl/minishell.h"
 
-int mini_cd(char **argv, int fd, t_shell *s)
+static int	do_chdir(char *path)
 {
-	int i;
-	char *path;
+	if (path[0] == '-' && path[1] == '\0')
+		path = getenv("OLDPWD");
+	if (chdir(path) != 0)
+	{
+		perror("minishell: cd");
+		return (1);
+	}
+	return (0);
+}
+
+int	mini_cd(char **argv, int fd, t_shell *s)
+{
+	int			i;
+	char		*path;
 
 	(void)fd;
 	(void)s;
@@ -11,22 +23,20 @@ int mini_cd(char **argv, int fd, t_shell *s)
 	while (argv[i])
 		i++;
 	if (i > 2)
-		return 1;
-    if (!argv[1])
-    {
-        path = getenv("HOME");
-        if (!path)
-        {
-            fprintf(stderr, "cd: HOME not set\n");
-            return 1;
-        }
-    }
-    else
-    	path = argv[1];
-    if (chdir(path) != 0)
-    {
-        perror("cd");
-        return 1;
-    }
-    return 0;
+	{
+		ft_fprintf(2, "minishell: cd: too many arguments\n");
+		return (1);
+	}
+	if (!argv[1])
+	{
+		path = getenv("HOME");
+		if (!path)
+		{
+			ft_fprintf(2, "minishell: cd: HOME not set\n");
+			return (1);
+		}
+	}
+	else
+		path = argv[1];
+	return (do_chdir(path));
 }
