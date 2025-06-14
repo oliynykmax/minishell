264c05e2c5f76b1ffd414284957ee2a66316dc15
oliynykmax <olejnikmax@gmail.com>
2025-06-14 15:06:26 +0300

    file structure for builtins

--- srcs/bn_exit_env.c
+++ srcs/bn_exit_env.c
@@ -51,3 +51,44 @@ int	mini_exit(char **args, int fd, t_shell *s)
 	shell_exit(s, (unsigned char)ft_atoi(args[1]), NULL);
 	return (0);
 }
+
+/*
+ * set invalid is used by
+ * export command
+ */
+int	set_invalid(char *var, int *status)
+{
+	ft_fprintf(2, "minishell: export: `%s': not a valid identifier\n", var);
+	*status = 1;
+	return (0);
+}
+
+/*
+ * prints out all the envp variables that has some value,
+ * not just key
+ */
+int	mini_env(char **argv, int fd, t_shell *s)
+{
+	int		i;
+	char	**env;
+
+	(void)argv;
+	if (s == NULL)
+		return (1);
+	if (s->envp == NULL)
+		return (1);
+	env = (char **)s->envp->data;
+	if (env == NULL)
+		return (0);
+	i = 0;
+	while (env[i] != NULL)
+	{
+		if (ft_strchr(env[i], '='))
+		{
+			ft_putstr_fd(env[i], fd);
+			ft_putstr_fd("\n", fd);
+		}
+		i++;
+	}
+	return (0);
+}
